b72d348ad46d9474c66de55aba1d8341240bc8b4
Levi Noecker <levi.noecker@gmail.com>
2021-12-05 09:33:12 -0600

    Dec 4 Part 1 initial pass working

--- 2021/04/bingo.go
+++ 2021/04/bingo.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"sync"
 )
 
 type (
@@ -11,8 +12,13 @@ type (
 	column map[string]bool
 )
 
-func NewBoard(raw []string) *Board {
-	board := &Board{}
+func NewBoard(id int, wg *sync.WaitGroup, newNumCh chan string, bingoCh chan int, raw []string) *Board {
+	board := new(Board)
+	board.id = id
+	board.wg = wg
+	board.raw = raw
+	board.newNumCh = newNumCh
+	board.bingoCh = bingoCh
 	board.rows = make([]row, 5)
 	board.columns = make([]column, 5)
 	for i := 0; i < 5; i++ {
@@ -30,12 +36,58 @@ func NewBoard(raw []string) *Board {
 		}
 	}
 
+	go board.Start()
+
 	return board
 }
 
 type Board struct {
-	rows    []row
-	columns []column
+	id       int
+	raw      []string
+	wg       *sync.WaitGroup
+	newNumCh chan string
+	bingoCh  chan int
+	rows     []row
+	columns  []column
+}
+
+func (b *Board) Start() {
+	for {
+		newNum := <-b.newNumCh
+		for _, r := range b.rows {
+			if _, ok := r[newNum]; ok {
+				r[newNum] = true
+			}
+		}
+		for _, c := range b.columns {
+			if _, ok := c[newNum]; ok {
+				c[newNum] = true
+			}
+		}
+
+	rowLoop:
+		for _, r := range b.rows {
+			for _, v := range r {
+				if v != true {
+					continue rowLoop
+				}
+			}
+			b.bingoCh <- b.id
+			break
+		}
+
+	columnLoop:
+		for _, c := range b.columns {
+			for _, v := range c {
+				if !v {
+					continue columnLoop
+				}
+			}
+			b.bingoCh <- b.id
+			break
+		}
+		b.wg.Done()
+	}
 }
 
 func (b *Board) GetRow(n int) (row, error) {
@@ -52,21 +104,90 @@ func (b *Board) GetColumn(n int) (column, error) {
 	return b.columns[n], nil
 }
 
-func LoadInput(input string) ([]string, []*Board) {
+func LoadInput(input string) (*sync.WaitGroup, chan int, []string, map[int]*Board) {
+
+	wg := new(sync.WaitGroup)
+
+	newNumCh := make(chan string)
+	bingoCh := make(chan int)
+
 	splitInput := strings.Split(input, "\n")
 
 	bingoNumbers := strings.Split(splitInput[0], ",")
 
-	boards := make([]*Board, 0)
 	boardsRaw := splitInput[2:]
 
-	for len(boardsRaw) > 0 {
+	boards := make(map[int]*Board)
+
+	boardId := 0
+	for len(boardsRaw) >= 5 {
 		b := boardsRaw[:5]
 		if len(boardsRaw) > 5 {
 			boardsRaw = boardsRaw[6:]
+		} else {
+			boardsRaw = boardsRaw[5:]
 		}
 
-		boards = append(boards, NewBoard(b))
+		boards[boardId] = NewBoard(boardId, wg, newNumCh, bingoCh, b)
+
+		boardId++
 	}
-	return bingoNumbers, boards
+	return wg, bingoCh, bingoNumbers, boards
+}
+
+func PlayBingo(input string) (*Board, error) {
+	wg, bingoCh, numbers, boards := LoadInput(input)
+
+	boardCount := len(boards)
+	fmt.Printf("Loaded %d boards\n", boardCount)
+
+	for _, num := range numbers {
+		// Do broadcast here
+
+		wg.Add(boardCount)
+		noBingo := make(chan struct{})
+		go func() {
+			wg.Wait()
+			close(noBingo)
+		}()
+
+		for _, b := range boards {
+			b.newNumCh <- num
+		}
+
+		select {
+		case bingo := <-bingoCh:
+			fmt.Printf("Board %d reported a bingo!\n", bingo)
+			return boards[bingo], nil
+		case <-noBingo:
+			continue
+		}
+	}
+
+	// TODO: Return error if we get here, ran out of numbers with no winner
+	return &Board{}, errors.New("No board got a bingo")
+}
+
+func main() {
+	input := `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7`
+
+	PlayBingo(input)
 }
--- 2021/04/bingo_test.go
+++ 2021/04/bingo_test.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"sync"
 	"testing"
+)
+
+var (
+	dummyWg      *sync.WaitGroup
+	dummyNumCh   = make(chan string)
+	dummyBingoCh = make(chan int)
 )
 
 func TestNewBoard(t *testing.T) {
@@ -16,7 +23,7 @@ func TestNewBoard(t *testing.T) {
 	}
 
 	t.Run("Rows", func(t *testing.T) {
-		board := NewBoard(boardRaw)
+		board := NewBoard(0, dummyWg, dummyNumCh, dummyBingoCh, boardRaw)
 		want := []map[string]bool{
 			map[string]bool{"22": false, "13": false, "17": false, "11": false, "0": false},
 			map[string]bool{"8": false, "2": false, "23": false, "4": false, "24": false},
@@ -39,7 +46,7 @@ func TestNewBoard(t *testing.T) {
 	})
 
 	t.Run("Columns", func(t *testing.T) {
-		board := NewBoard(boardRaw)
+		board := NewBoard(0, dummyWg, dummyNumCh, dummyBingoCh, boardRaw)
 		want := []map[string]bool{
 			map[string]bool{"22": false, "8": false, "21": false, "6": false, "1": false},
 			map[string]bool{"13": false, "2": false, "9": false, "10": false, "12": false},
@@ -73,7 +80,7 @@ func TestLoadInput(t *testing.T) {
 `
 
 	t.Run("load numbers", func(t *testing.T) {
-		got, _ := LoadInput(input)
+		_, _, got, _ := LoadInput(input)
 		want := []string{"7", "4", "9", "5", "11", "17", "23", "2", "0", "14", "21", "24", "10", "16", "13", "6", "15", "25", "12", "22", "18", "20", "8", "19", "3", "26", "1"}
 
 		if !reflect.DeepEqual(got, want) {
@@ -82,8 +89,8 @@ func TestLoadInput(t *testing.T) {
 	})
 
 	t.Run("load boards", func(t *testing.T) {
-		_, got := LoadInput(input)
-		want := NewBoard([]string{
+		_, _, _, got := LoadInput(input)
+		want := NewBoard(0, dummyWg, dummyNumCh, dummyBingoCh, []string{
 			"22 13 17 11  0",
 			" 8  2 23  4 24",
 			"21  9 14 16  7",
@@ -91,8 +98,51 @@ func TestLoadInput(t *testing.T) {
 			" 1 12 20 15 19",
 		})
 
-		if !reflect.DeepEqual(got[0], want) {
-			t.Errorf("got %v, want %v", got[0], want)
+		if !reflect.DeepEqual(got[0].rows, want.rows) {
+			t.Errorf("got %v, want %v", got[0].rows, want.columns)
+		}
+
+		if !reflect.DeepEqual(got[0].columns, want.columns) {
+			t.Errorf("got %v, want %v", got[0].rows, want.columns)
 		}
 	})
+}
+
+func TestBingo(t *testing.T) {
+	input := `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7`
+
+	got, err := PlayBingo(input)
+	want := NewBoard(2, dummyWg, dummyNumCh, dummyBingoCh, []string{
+		"14 21 17 24  4",
+		"10 16 15  9 19",
+		"18  8 23 26 20",
+		"22 11 13  6  5",
+		" 2  0 12  3  7",
+	})
+
+	if err != nil {
+		t.Fatal("Got an unexpected error: ", err)
+	}
+
+	if fmt.Sprint(got.raw) != fmt.Sprint(want.raw) {
+		t.Errorf("got %v, want %v", got.raw, want.raw)
+	}
 }

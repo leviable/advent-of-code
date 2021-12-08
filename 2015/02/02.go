fe970ca462ed17ab15167cb782c10149d5766b38
Levi Noecker <levi.noecker@gmail.com>
2021-12-08 13:28:00 -0600

    2015 Day 2 Part 2 complete

--- 2015/02/02.go
+++ 2015/02/02.go
@@ -4,35 +4,47 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-func CalculatePaper(dimensions string) (area int) {
-	d := strings.Split(dimensions, "x")
-
-	l, w, h := toInt(d[0]), toInt(d[1]), toInt(d[2])
+func CalculatePaper(dimensions string) int {
+	l, w, h := GetDimensions(dimensions)
 
 	side1 := l * w
 	side2 := l * h
 	side3 := w * h
 
-	var extra int
-	if side1 <= side2 && side1 <= side3 {
-		extra = side1
-	} else if side2 <= side1 && side2 <= side3 {
-		extra = side2
-	} else {
-		extra = side3
-	}
+	extra, _ := GetSmallest(side1, side2, side3)
 
 	return 2*side1 + 2*side2 + 2*side3 + extra
 }
 
-func toInt(num string) int {
-	n, _ := strconv.Atoi(num)
+func CalculateRibbon(dimensions string) int {
+	l, w, h := GetDimensions(dimensions)
+	smallest1, smallest2 := GetSmallest(l, w, h)
+	ribbon := 2*smallest1 + 2*smallest2
+	bow := l * w * h
+	return ribbon + bow
+}
 
-	return n
+func GetDimensions(dimensions string) (int, int, int) {
+
+	d := strings.Split(dimensions, "x")
+	toInt := func(num string) int {
+		n, _ := strconv.Atoi(num)
+		return n
+	}
+
+	return toInt(d[0]), toInt(d[1]), toInt(d[2])
+}
+
+func GetSmallest(s1, s2, s3 int) (int, int) {
+	sides := []int{s1, s2, s3}
+	sort.Ints(sides)
+
+	return sides[0], sides[1]
 }
 
 func main() {
@@ -52,10 +64,12 @@ func main() {
 		panic(fmt.Sprint("Error scanning file: ", err))
 	}
 
-	total := 0
+	totalPaper, totalRibbon := 0, 0
 	for _, d := range input {
-		total += CalculatePaper(d)
+		totalPaper += CalculatePaper(d)
+		totalRibbon += CalculateRibbon(d)
 	}
 
-	fmt.Println("Total paper required: ", total)
+	fmt.Println("Total paper required: ", totalPaper)
+	fmt.Println("Total ribbon required: ", totalRibbon)
 }
--- 2015/02/02_test.go
+++ 2015/02/02_test.go
@@ -8,13 +8,34 @@ func TestCalculatePaper(t *testing.T) {
 		want       int
 	}{
 		{dimensions: "2x3x4", want: 58},
-		{dimensions: "1x1x10", want: 42},
+		{dimensions: "1x1x10", want: 43},
 	}
 
 	for _, tt := range examples {
 		t.Run(tt.dimensions, func(t *testing.T) {
-			got := CalculatePaper("2x3x4")
-			want := 58
+			got := CalculatePaper(tt.dimensions)
+			want := tt.want
+
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
+	}
+}
+
+func TestCalculateRibbon(t *testing.T) {
+	examples := []struct {
+		dimensions string
+		want       int
+	}{
+		{dimensions: "2x3x4", want: 34},
+		{dimensions: "1x1x10", want: 14},
+	}
+
+	for _, tt := range examples {
+		t.Run(tt.dimensions, func(t *testing.T) {
+			got := CalculateRibbon(tt.dimensions)
+			want := tt.want
 
 			if got != want {
 				t.Errorf("got %d, want %d", got, want)
